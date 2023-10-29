9a66aebac85cdf4dacb0f00ad7bfd68034bcfe07
Frank <1155185406@link.cuhk.edu.hk>
2023-10-29 12:31:27 +0800

    get each arm average reward

--- arm_selection.py
+++ arm_selection.py
@@ -10,7 +10,6 @@ class ArmSelection():
 
 
 
-
     def create_arms(self):
         for i in range(self.N):
             self.arms.append(i + 1)
@@ -21,5 +20,9 @@ class ArmSelection():
         return np.random.uniform(0, arm)   # arm 1 will be uniformly distributed in [0, 1] and arm 2 may be uniformly distributed in [0, 2], etc.
 
     
-    def get_arms(self):
-        return self.arms
+    def get_average_reward(self,arm:int, N: int = 1000) -> float:
+        total_reward = 0
+        for i in range(N):
+            total_reward += self.get_uniform_distribution_reward(arm)
+        return total_reward / N
+        
