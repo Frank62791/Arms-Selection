d1f8e2e2f4ffc68c6fb73076cefe493a0b73966e
Frank <1155185406@link.cuhk.edu.hk>
2023-11-04 16:30:29 +0800

    fix: T variable

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
 from arm_selection import ArmSelection
 
-
+T = 10000               #change  T = 1000 and T = 1500 and T = 2000
 armSelection = ArmSelection()
-print(armSelection.random_policy(T=10000))                                                                   #change  T = 1000 and T = 1500 and T = 2000
-print(armSelection.ETE_policy(T=10000))
-print(armSelection.greedy_policy(T=10000))
-print(armSelection.adaptive_epsilon_greedy_policy(T=10000))
-print(armSelection.upper_confidence_bound_policy(T=10000))
+print(armSelection.random_policy(T=T))                                                                 
+print(armSelection.ETE_policy(T=T))
+print(armSelection.greedy_policy(T=T))
+print(armSelection.adaptive_epsilon_greedy_policy(T=T))
+print(armSelection.upper_confidence_bound_policy(T=T))
 
 
 
